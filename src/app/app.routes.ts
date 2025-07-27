9c5b5d9bbb12342904a088eef3eb6c4e88239b62
Cleyton Alves <cleyton.alves.a@gmail.com>
2025-07-27 14:19:50 -0300

    feat: :sparkles: Estrutura de login completa com style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { USER_ROUTES } from './features/client/pages/user/user.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'user',
+    children: USER_ROUTES
+  }
+];
