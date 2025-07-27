dd0c58ca2e1d906a29f401b87345f1d0f95f8d67
Cleyton Alves <cleyton.alves.a@gmail.com>
2025-07-27 11:30:39 -0300

    chore: :tada: Iniciando front end da Easy Travel - ET

---
+++ src/app/features/client/pages/bundle/bundle.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bundle',
+  imports: [],
+  templateUrl: './bundle.html',
+  styleUrl: './bundle.css'
+})
+export class Bundle {
+
+}
