c002ddc178100ee04953093732b2fcf21704c3fc
AllanRDS Avanade <a.reymond.da.silva@avanade.com>
2025-07-28 19:18:38 -0300

    Revert "Merge branch 'main' of https://github.com/Squad-01-EasyTravel/EasyTravel-Client into teste"
    This reverts commit 76e627bae1fbee4edc9474d81ec7fd658e41a62e, reversing
    changes made to 9c63aa6f9772412dbb7bd91fb96a028052ab2ce4.

--- src/app/features/client/pages/home/home.ts
+++ src/app/features/client/pages/home/home.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { Footer } from '../../../../shared/footer/footer';
 import { Navbar } from '../../../../shared/navbar/navbar';
-import { TripCarousel } from '../../../../shared/trip-carousel/trip-carousel';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [Footer, Navbar, TripCarousel],
+  imports: [Footer, Navbar],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
