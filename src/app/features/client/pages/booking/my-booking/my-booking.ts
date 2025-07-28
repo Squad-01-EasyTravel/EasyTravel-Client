7e406ee5f4b9947c628be835b662df3363f3e718
AllanRDS Avanade <a.reymond.da.silva@avanade.com>
2025-07-28 17:15:42 -0300

    feat: :sparkles: Configurações de rotas e importação pra minhas reservas

--- src/app/features/client/pages/booking/my-booking/my-booking.ts
+++ src/app/features/client/pages/booking/my-booking/my-booking.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Navbar } from '../../../../../shared/navbar/navbar';
+import { Footer } from '../../../../../shared/footer/footer';
 
 @Component({
   selector: 'app-my-booking',
-  imports: [],
+  imports: [Navbar, Footer],
   templateUrl: './my-booking.html',
   styleUrl: './my-booking.css'
 })
