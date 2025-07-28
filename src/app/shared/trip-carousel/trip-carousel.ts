071ae330b60305e37c19c27da1b59da6462a9e90
Cleyton Alves <cleyton.alves.a@gmail.com>
2025-07-28 18:58:59 -0300

    chore: :construction: Cards em progresso

--- src/app/shared/trip-carousel/trip-carousel.ts
+++ src/app/shared/trip-carousel/trip-carousel.ts
@@ -1,23 +1,74 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common'; // ✅ Importa os pipes comuns
 import { Trip } from '../models/trip.interface';
-import { SwiperModule } from 'swiper/angular';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-trip-carousel',
   standalone: true,
-  imports: [CommonModule, SwiperModule],
+  imports: [CommonModule], // ✅ Adiciona o CommonModule aos imports
   templateUrl: './trip-carousel.html',
-  styleUrls: ['./trip-carousel.css'],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  styleUrls: ['./trip-carousel.css']
 })
 export class TripCarousel {
   trips: Trip[] = [
     {
       imageUrl: 'assets/imgs/fortaleza.jpg',
       origin: 'São Paulo',
-      destination: 'Recife',
+      destination: 'Fortaleza',
+      price: 320.00,
+      departureDate: '2025-08-10',
+      arrivalDate: '2025-08-15',
+      rating: 4.7
+    },
+    {
+      imageUrl: 'assets/imgs/gramado.jpg',
+      origin: 'Rio de Janeiro',
+      destination: 'Gramado',
+      price: 450.00,
+      departureDate: '2025-09-05',
+      arrivalDate: '2025-09-12',
+      rating: 4.9
+    },
+     {
+      imageUrl: 'assets/imgs/fortaleza.jpg',
+      origin: 'São Paulo',
+      destination: 'Fortaleza',
+      price: 320.00,
+      departureDate: '2025-08-10',
+      arrivalDate: '2025-08-15',
+      rating: 4.7
+    },
+    {
+      imageUrl: 'assets/imgs/gramado.jpg',
+      origin: 'Rio de Janeiro',
+      destination: 'Gramado',
+      price: 450.00,
+      departureDate: '2025-09-05',
+      arrivalDate: '2025-09-12',
+      rating: 4.9
+    },
+     {
+      imageUrl: 'assets/imgs/fortaleza.jpg',
+      origin: 'São Paulo',
+      destination: 'Fortaleza',
+      price: 320.00,
+      departureDate: '2025-08-10',
+      arrivalDate: '2025-08-15',
+      rating: 4.7
+    },
+    {
+      imageUrl: 'assets/imgs/gramado.jpg',
+      origin: 'Rio de Janeiro',
+      destination: 'Gramado',
+      price: 450.00,
+      departureDate: '2025-09-05',
+      arrivalDate: '2025-09-12',
+      rating: 4.9
+    },
+     {
+      imageUrl: 'assets/imgs/fortaleza.jpg',
+      origin: 'São Paulo',
+      destination: 'Fortaleza',
       price: 320.00,
       departureDate: '2025-08-10',
       arrivalDate: '2025-08-15',
