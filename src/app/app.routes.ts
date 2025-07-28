a34267319cd804c083a17f67c9ea4fd880babb54
AllanRDS Avanade <a.reymond.da.silva@avanade.com>
2025-07-28 17:18:52 -0300

    refactor: :bug: Corrigindo importação da rota auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/client/pages/home/home';
 import { AUTH_ROUTES } from './features/client/pages/auth/auth.routes';
-import { AUTH_ROUTES } from './features/client/pages/auth/user.routes';
 import { MyBooking } from './features/client/pages/booking/my-booking/my-booking';
 
 export const routes: Routes = [
