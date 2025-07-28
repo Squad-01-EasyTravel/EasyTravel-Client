91babf723bab4b48ebe97320958308eb01adb67f
AllanRDS Avanade <a.reymond.da.silva@avanade.com>
2025-07-28 16:44:57 -0300

    feat: :sparkles: Criação da estrutura de minhas reservas

---
+++ src/app/features/client/pages/booking/my-booking/my-booking.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-booking',
+  imports: [],
+  templateUrl: './my-booking.html',
+  styleUrl: './my-booking.css'
+})
+export class MyBooking {
+
+}
