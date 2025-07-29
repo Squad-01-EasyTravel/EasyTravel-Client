d9e934cec3e4ae2021ac810b3ba2dcf498015f99
JonTalmon <jonatas_talmon@hotmail.com>
2025-07-29 16:08:09 -0300

    Finishing profile tab

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 
 import { Routes } from '@angular/router';
 import { Home } from './features/client/pages/home/home';
+import { Profile } from './features/client/pages/user/profile/profile';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'home', component: Home },
-  { path: 'user', children: USER_ROUTES }
+  { path: 'profile', component: Profile}
 ];
