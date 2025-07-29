1e074ed588118d365c1d1714920c7ef0fde6d0a1
Davi Gomes da Silva <davi.gomes.da.silva@avanade.com>
2025-07-29 08:55:27 -0300

    feat: :package: adicionando tela de exibição de pacotes de viagem

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@
 import { Routes } from '@angular/router';
 import { USER_ROUTES } from './features/client/pages/user/user.routes';
 import { Home } from './features/client/pages/home/home';
+import { Bundle } from './features/client/pages/bundle/bundle';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'home', component: Home },
+  { path: 'bundles', component: Bundle },
   { path: 'user', children: USER_ROUTES }
 ];
