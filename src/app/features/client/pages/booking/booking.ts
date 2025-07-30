5b7be38fc3ee4355adf892764fc390f110900eca
Davi Miqueias <miqueiasdavi8@gmail.com>
2025-07-30 11:45:34 -0300

    feat: :zap: implementação completa da pagina reservas

--- src/app/features/client/pages/booking/booking.ts
+++ src/app/features/client/pages/booking/booking.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Selected } from "./selected/selected";
+import { PurchaseSummary } from "./purchase-summary/purchase-summary";
+import { Navbar } from "../../../../shared/navbar/navbar";
 
 @Component({
   selector: 'app-booking',
-  imports: [Selected],
+  imports: [Selected, PurchaseSummary, Navbar],
   templateUrl: './booking.html',
   styleUrl: './booking.css'
 })
