82fb3bb223db5110302e8797148242637e05a9ee
JonTalmon <jonatas_talmon@hotmail.com>
2025-07-29 22:44:07 -0300

    adjusting routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/client/pages/home/home';
 import { Profile } from './features/client/pages/user/profile/profile';
+import { PayInfo } from './features/client/pages/user/pay-info/pay-info';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'home', component: Home },
-  { path: 'profile', component: Profile}
+  { path: 'profile', component: Profile},
+  { path: 'profile/paymentinformation', component: PayInfo},
 ];
